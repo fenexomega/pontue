288c747e05b101305b7846ade31ee2d974a26f92
Jordy <jordyfgomes@gmail.com>
2017-01-24 18:38:56 -0300

    initial commit

---
+++ js/controllers/PontoController.js
@@ -0,0 +1,13 @@
+'use strict';
+var controllers = angular.module('mainApp.controllers');
+var URL = "http://localhost:8080";
+var timeoutRef;
+
+function PontoController($scope,IraService)
+{
+
+}
+
+controllers.controller('PontoController',PontoController);
+
+PontoController.$inject = ['$scope'];
---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var controllers = angular.module('mainApp.controllers',[]);
+// var services    = angular.module('mainApp.services',[]);
+// var directives  = angular.module('mainApp.directives',[]);
+// var constants   = angular.module('mainApp.constants',[]);
+
+var app = angular.module("mainApp",[
+  /* Módulos da aplicação */
+  'ngRoute',
+  /* Nossos módulos */
+  'mainApp.controllers' //,'mainApp.services', 'mainApp.directives', 'mainApp.constants'
+]);
+
+app.config(function($routeProvider,$locationProvider){
+  $routeProvider
+  .when('/',{
+    templateUrl: 'partials/inicio.html',
+    controller: 'PontoController.js'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+  $locationProvider.html5Mode(true);
+})
