52a59faedbcff98aeec4707c6220db5f6ee599d6
Jordy <jordyfgomes@gmail.com>
2017-01-25 18:26:30 -0300

    update

--- js/controllers/PontoController.js
+++ js/controllers/PontoController.js
@@ -5,7 +5,51 @@ var timeoutRef;
 
 function PontoController($scope,PontoService)
 {
+  $scope.pegarHorasDaSemana = function(){
+    PontoService.pegarPontosDaSemana(token,
+    function(data,status){
+      $scope.horasSemana = data.horas;
+      $scope.semana = data.semana;
+    },
+    //On Error
+    function(data,status){
+      // TODO o que acontece quando dá erro
+    }
+  );
 
+  $scope.submeterHorasDoDia = function(objeto)
+  // Objeto é um json no seguinte formato
+  /*
+   *  {
+   *    "comentario":"",
+   *     "horarios":{
+   *      "manha":{
+   *      "ab":0,
+   *      "cd":0
+   *      },
+   *      "tarde":{
+   *      "ab":0,
+   *      "cd":0
+   *      },
+   *      "noite":{
+   *      "ab":0,
+   *      "cd":0
+   *      }
+   *    }
+   *  }
+   */
+  {
+    PontoService.enviarPonto(objeto,
+    function(data,status){
+
+    },
+    function(data,status){
+      
+    });
+  }
+
+
+  }
 }
 
 controllers.controller('PontoController',PontoController);
