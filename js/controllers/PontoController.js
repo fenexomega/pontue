afb68144460832a0f6b29ba4dfe95c24f58c0345
Jordy <jordyfgomes@gmail.com>
2017-02-01 19:24:43 -0300

    update: ng-click não estava funcionando. Agradeço ao usuário rans0m do canal #angularjs @freenode pela ajuda. Thanks for you help! :)

--- js/controllers/PontoController.js
+++ js/controllers/PontoController.js
@@ -1,126 +1,156 @@
 'use strict';
 var controllers = angular.module('mainApp.controllers');
 
-function PontoController($scope, $interval, PontoService, moment)
-{
+function PontoController($scope, $interval, PontoService, moment) {
 
-  function incrementarData()
-  {
-    $scope.dataDeHoje.setSeconds($scope.dataDeHoje.getSeconds() + 1);
-    var myMoment = moment($scope.dataDeHoje);
-    $scope.hora = myMoment.format('h:mm:ss');
-  };
+    var incrementarData = function() {
+        $scope.dataDeHoje.setSeconds($scope.dataDeHoje.getSeconds() + 1);
+        var myMoment = moment($scope.dataDeHoje);
+        $scope.hora = myMoment.format('h:mm:ss');
+    };
 
-  function configChart()
-  {
-    $scope.myChartObject = {};
-    $scope.myChartObject.type = "BarChart";
+    var configChart = function() {
+        $scope.myChartObject = {};
+        $scope.myChartObject.type = "BarChart";
+        $scope.myChartObject.data = {
+            "cols": [{
+                    id: "dia",
+                    label: "Dias da Semana",
+                    type: "string"
+                },
+                {
+                    id: "horas",
+                    label: "Total de Horas",
+                    type: "number"
+                }
+            ],
+            "rows": [{
+                    c: [{
+                            v: "Domingo"
+                        },
+                        {
+                            v: 2
+                        }
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Segunda"
+                        },
+                        {
+                            v: 2
+                        }
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Terça"
+                        },
+                        {
+                            v: 4
+                        }
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Quarta"
+                        },
+                        {
+                            v: 2
+                        },
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Quinta"
+                        },
+                        {
+                            v: 2
+                        },
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Sexta"
+                        },
+                        {
+                            v: 1
+                        },
+                    ]
+                },
+                {
+                    c: [{
+                            v: "Sábado"
+                        },
+                        {
+                            v: 2
+                        },
+                    ]
+                }
+            ]
+        };
+        $scope.myChartObject.options = {
+            'title': 'Total de Horas por Dia da Semana'
+        };
+    };
 
-    $scope.myChartObject.data = {
-      "cols": [{id: "dia", label: "Dias da Semana", type: "string"},
-               {id: "horas", label: "Total de Horas", type: "number"}],
-      "rows": [
-        {c: [
-            {v: "Domingo"},
-            {v: 2}
-        ]},
-        {c: [
-          {v: "Segunda"},
-          {v: 2}
-        ]},
-        {c: [
-            {v: "Terça"},
-            {v: 4}
-        ]},
-        {c: [
-            {v: "Quarta"},
-            {v: 2},
-        ]},
-        {c: [
-            {v: "Quinta"},
-            {v: 2},
-        ]},
-        {c: [
-            {v: "Sexta"},
-            {v: 1},
-        ]},
-        {c: [
-            {v: "Sábado"},
-            {v: 2},
-        ]}
-      ]};
-      $scope.myChartObject.options = {
-          'title': 'Total de Horas por Dia da Semana'
-      };
-  };
-
-
-
-  $scope.init = function()
-  {
-    console.log("Init");
     $scope.dataDeHoje = new Date();
+
     var myMoment = moment($scope.dataDeHoje).locale('pt-br');
+
     $scope.myMoment = myMoment;
     $scope.dia_semana = myMoment.format('dddd');
     $scope.data_formatada = myMoment.format('LL');
 
-
     incrementarData();
-    $interval(incrementarData,1000);
+    $interval(incrementarData, 1000);
     configChart();
-  };
 
+    $scope.Enviar = function(){
+        alert("Enviar");
+    };
 
-  $scope.pegarHorasDaSemana = function()
-  {
-    console.log("Pegando horas da semana!");
-    PontoService.pegarPontosDaSemana(token,
-    function(data,status){
-      $scope.horasSemana = data.horas;
-      $scope.semana = data.semana;
-    },
-    //On Error
-    function(data,status){
-      // TODO o que acontece quando dá erro
-    });
-  };
+    $scope.afsdfds = function(){
+        alert("afsdfds");
+    };
 
+    $scope.pegarHorasDaSemana = function() {
+        console.log("Pegando horas da semana!");
+        PontoService.pegarPontosDaSemana(token,
+            function(data, status) {
+                $scope.horasSemana = data.horas;
+                $scope.semana = data.semana;
+            },
+            //On Error
+            function(data, status) {
+                // TODO o que acontece quando dá erro
+            });
+    };
 
-  $scope.submeterHorasDoDia = function(objeto)
-  {
-    // Objeto é um json no seguinte formato
-    /*
-     *  {
-     *    "comentario":"",
-     *     "horarios":{
-     *      "manha":{
-     *      "ab":0,
-     *      "cd":0
-     *      },
-     *      "tarde":{
-     *      "ab":0,
-     *      "cd":0
-     *      },
-     *      "noite":{
-     *      "ab":0,
-     *      "cd":0
-     *      }
-     *    }
-     *  }
-     */
-    console.log("Submeter horas!");
-    console.log(objeto);
-    return;
-    PontoService.enviarPonto(objeto,
-    function(data,status){
-
-    },
-    function(data,status){
-
-    });
-  };
+    $scope.submeterHorasDoDia = function(objeto) {
+        // Objeto é um json no seguinte formato
+        /*
+         *  {
+         *    "comentario":"",
+         *     "horarios":{
+         *      "manha":{
+         *      "ab":0,
+         *      "cd":0
+         *      },
+         *      "tarde":{
+         *      "ab":0,
+         *      "cd":0
+         *      },
+         *      "noite":{
+         *      "ab":0,
+         *      "cd":0
+         *      }
+         *    }
+         *  }
+         */
+        PontoService.enviarPonto(objeto, function(data, status) {}, function(data, status) {});
+    };
 };
 
-PontoController.$inject = ['$scope','$interval','PontoService', 'moment'];
+PontoController.$inject = ['$scope', '$interval', 'PontoService', 'moment'];
 controllers.controller('PontoController', PontoController);
